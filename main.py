ae32f9803a77b162ef45d7f9fe4eeb9b84b588c9
Rohit Suratekar <rohitsuratekar@gmail.com>
2019-07-27 09:48:30 +0200

    Initial Commit

---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+Author : Rohit Suratekar
+Data : July 2019
+
+Data analysis on the MovieLens data-set
+
+( https://grouplens.org/datasets/movielens/ )
+
+Acknowledgment :
+
+F. Maxwell Harper and Joseph A. Konstan. 2015. The MovieLens Datasets: History
+and Context. ACM Transactions  on Interactive Intelligent Systems (TiiS) 5,
+4, Article 19 (December 2015),  19 pages.
+DOI=<http://dx.doi.org/10.1145/2827872>
+
+Dataset was accessed on 27 July 2019
+
+"""
