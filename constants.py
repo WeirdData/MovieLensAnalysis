adbb1bd875cb1fb5962b6f0e4a4b1eb51505a319
Rohit Suratekar <rohitsuratekar@gmail.com>
2020-09-26 19:26:23 +0200

    Cosine-distance

---
+++ constants.py
@@ -0,0 +1,13 @@
+#  Copyright (c) 2020.  WeirdData
+#  Author: Rohit Suratekar
+#
+#  All constants related to this analysis
+
+MOVIE_ID = "movieId"
+MOVIE_TITLE = "title"
+MOVIE_GENRES = "genres"
+USER_ID = "userId"
+RATING = "rating"
+TIMESTAMP = "timestamp"
+TAG = "tag"
+YEAR = "year"
--- main.py
+++ main.py
@@ -1,32 +1,10 @@
-"""
-Author : Rohit Suratekar
-Data : July 2019
+#  Copyright (c) 2020.  WeirdData
+#  Author: Rohit Suratekar
+#
+#  Dataset used: MovieLens 20M
+#  https://www.kaggle.com/grouplens/movielens-20m-dataset
 
-Data analysis on the MovieLens data-set
 
-( https://grouplens.org/datasets/movielens/ )
+from distance_wise import run
 
-Acknowledgment :
-
-F. Maxwell Harper and Joseph A. Konstan. 2015. The MovieLens Datasets: History
-and Context. ACM Transactions  on Interactive Intelligent Systems (TiiS) 5,
-4, Article 19 (December 2015),  19 pages.
-DOI=<http://dx.doi.org/10.1145/2827872>
-
-Dataset was accessed on 27 July 2019
-
-"""
-
-import os
-
-from analysis.clustering import run
-from constants.system import SAVE_FOLDER, OUT_FOLDER
-
-# Create folder for saving plots
-if not os.path.exists(SAVE_FOLDER):
-    os.makedirs(SAVE_FOLDER)
-
-# Create folder for saving output files
-if not os.path.exists(OUT_FOLDER):
-    os.makedirs(OUT_FOLDER)
 run()
